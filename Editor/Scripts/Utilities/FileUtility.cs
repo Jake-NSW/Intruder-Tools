ac695b9eef00ea5539a4a91f08daf5fb42f10cfe
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2021-08-24 13:46:38 +1000

    Added file size comparer

--- Editor/Scripts/Utilities/FileUtility.cs
+++ Editor/Scripts/Utilities/FileUtility.cs
@@ -2,6 +2,11 @@ using System.IO;
 
 namespace Intruder.Tools
 {
+	public enum FileSize
+	{
+		Bytes, Kilobytes, Megabytes
+	}
+
 	public static class FileUtility
 	{
 		public static void DirectoryCheck( string path )
@@ -10,9 +15,28 @@ namespace Intruder.Tools
 				Directory.CreateDirectory( path );
 		}
 
-		public static float GetFileSize( string path )
+		public static float GetFileSize( string path, FileSize fileSize )
 		{
-			return new FileInfo( path ).Length;
+			float bytes = new FileInfo( Path.GetFullPath( path ) ).Length;
+			var kilobytes = bytes / 1024;
+			var megabytes = kilobytes / 1024;
+
+			switch ( fileSize )
+			{
+				case FileSize.Bytes:
+					return bytes;
+				case FileSize.Kilobytes:
+					return kilobytes;
+				case FileSize.Megabytes:
+					return megabytes;
+				default:
+					return 0;
+			}
+		}
+
+		public static bool IsOverSize( string path, FileSize size, float value )
+		{
+			return GetFileSize( path, size ) > value;
 		}
 
 		public static void DeleteAllFilesWithExtensionAtPath( string path, string extension )
