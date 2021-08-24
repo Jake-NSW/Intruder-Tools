66776be8c228ce998e44b458650dbe1abfa799c8
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2021-08-25 01:19:00 +1000

    Added operating system module install checker
    Co-Authored-By: Rob Storm <11183957+robstorm@users.noreply.github.com>

---
+++ Editor/Scripts/Core/Initializer.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+namespace Intruder.Tools
+{
+	public class Initializer
+	{
+		[InitializeOnLoadMethod]
+		private static void InstallCheck()
+		{
+			HasAllUnityBuildPlatforms();
+		}
+
+		public static bool HasAllUnityBuildPlatforms()
+		{
+			var moduleManager = System.Type.GetType( "UnityEditor.Modules.ModuleManager,UnityEditor.dll" );
+			var isPlatformSupportLoaded = moduleManager.GetMethod( "IsPlatformSupportLoaded", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic );
+			var getTargetStringFromBuildTarget = moduleManager.GetMethod( "GetTargetStringFromBuildTarget", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic );
+
+			bool windowsLoaded = (bool)isPlatformSupportLoaded.Invoke( null, new object[]
+			{
+				(string) getTargetStringFromBuildTarget.Invoke(null, new object[] { BuildTarget.StandaloneWindows64 })
+			} );
+
+			bool osxLoaded = (bool)isPlatformSupportLoaded.Invoke( null, new object[]
+			{
+				(string) getTargetStringFromBuildTarget.Invoke(null, new object[] { BuildTarget.StandaloneOSX })
+			} );
+
+			if ( !(windowsLoaded && osxLoaded) )
+			{
+				string platformInstallError = "Error: Cannot build. Make sure both Windows and OSX build platforms are installed. Run Unity installer again to fix.";
+				Debug.LogError( platformInstallError );
+				EditorUtility.DisplayDialog( "Build error", platformInstallError, "Ok" );
+			}
+
+			return windowsLoaded && osxLoaded;
+		}
+	}
+}
