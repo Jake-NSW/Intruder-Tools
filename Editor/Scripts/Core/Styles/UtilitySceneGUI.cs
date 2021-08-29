b16002434a156ccc7ae87e6c90b1d8df83b66c16
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2021-08-29 14:25:53 +1000

    activator scene gui

--- Editor/Scripts/Core/Styles/UtilitySceneGUI.cs
+++ Editor/Scripts/Core/Styles/UtilitySceneGUI.cs
@@ -5,25 +5,31 @@ namespace Intruder.Tools.SceneGUI
 {
 	public static class UtilitySceneGUI
 	{
-		public static void DrawActivatorLine( Activator activator, Vector3 to, Color color = default( Color ) )
+		public static void DrawActivatorLine( Activator activator, Vector3 to, Color color = default( Color ), float width = 4 )
 		{
 			Handles.color = color;
 
 			var activatorPos = activator.transform.position;
-			var activeView = SceneView.lastActiveSceneView;
-
 			var direction = (activatorPos - to).normalized;
-			var distance = Vector3.Distance( activeView.camera.transform.position, activatorPos );
-			var size = 0.05f / distance / (2.0f * Mathf.Tan( (activeView.camera.fieldOfView / 2.0f) * Mathf.Deg2Rad )) * 1000.0f;
 
-			Handles.DrawAAPolyLine( size, activatorPos, to );
+			Handles.DrawLine( activatorPos, to, width );
 
-			for ( float i = 0; i < Vector3.Distance( activatorPos, to ); i++ )
+			for ( int i = 3; i < Vector3.Distance( to, activatorPos ); i += 2 )
 			{
-				Handles.CircleHandleCap( 0, activatorPos - direction * i, /* activatorPos + (direction * (i - 0.15f)) +  */Quaternion.FromToRotation( activatorPos, to ).normalized, 0.5f, EventType.Repaint );
+				Handles.ArrowHandleCap( 0, to + (direction * i), Quaternion.LookRotation( -direction, direction ), 1, EventType.Repaint );
 			}
 
+			ThickCircle( to, SceneView.lastActiveSceneView.camera.transform.rotation, color, width, 3 );
+
 			Handles.color = Color.white;
+		}
+
+		public static void ThickCircle( Vector3 pos, Quaternion rotation, Color color, float gap, float thickness = 2 )
+		{
+			for ( int i = 1; i < (thickness * 10); i++ )
+			{
+				Handles.CircleHandleCap( i, pos, rotation, (gap / 16) + (i * 0.0005f), EventType.Repaint );
+			}
 		}
 	}
 }
--- Editor/Scripts/Editors/Activator.Editor.cs
+++ Editor/Scripts/Editors/Activator.Editor.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,16 @@ namespace Intruder.Tools.Editors
 	[CustomEditor( typeof( Activator ) )]
 	public class ActivatorEditor : Editor
 	{
-		public override bool UseDefaultMargins() => false;
+		private Activator Activator => target as Activator;
 
 		public override void OnInspectorGUI()
 		{
 			base.OnInspectorGUI();
+		}
+
+		private void OnSceneGUI()
+		{
+			SceneGUI.UtilitySceneGUI.DrawActivatorLine( Activator, Activator.objectsToEnable[0].transform.position, Color.cyan );
 		}
 	}
 }
